b27fac4bc09b6d227a6134904384803ec33f27c0
Liam Choi <netcory@gmail.com>
2021-05-24 12:45:26 +0900

    Add files via upload

---
+++ 4. snail wants to climb.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def snail(A, B, V):
+    
+    import math
+        
+    
+    d = (V-B)/(A-B)
+       
+    
+    return math.ceil(d)
+ 
+snail(100, 99, 1000000000)
+
+
+출처: https://matsu53.tistory.com/8?category=0 [Matsu]
+
