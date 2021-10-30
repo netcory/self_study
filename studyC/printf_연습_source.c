7ef6e9c20286101f0ce043f857b3522998591e48
Liam Choi <netcory@gmail.com>
2021-10-30 22:07:37 +0900

    Rename 소스.c to printf_연습_source.c

---
+++ studyC/printf_연습_source.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("안\n녕\n하\n세\n요\n");
+	printf("안\t녕\t하\t세\t요\n");
+	printf("안녕하세요\b용\n");
+	printf("안녕하세요\r멍멍\n");
+	printf("감사합니다!");
+	system("pause");
+
+	return 0;
+}
