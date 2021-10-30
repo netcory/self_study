22717d049120042fe40a047454abcdad612238a1
Liam Choi <netcory@gmail.com>
2021-10-30 22:07:18 +0900

    Add files via upload

---
+++ studyC/소스.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("æ»\n≥Á\n«œ\nºº\nø‰\n");
+	printf("æ»\t≥Á\t«œ\tºº\tø‰\n");
+	printf("æ»≥Á«œººø‰\bøÎ\n");
+	printf("æ»≥Á«œººø‰\r∏€∏€\n");
+	printf("∞®ªÁ«’¥œ¥Ÿ!");
+	system("pause");
+
+	return 0;
+}
