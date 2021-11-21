361190c9036906ccc62cc1bab877d118876c18b4
Liam Choi <netcory@gmail.com>
2021-11-21 17:26:37 +0900

    Add files via upload

---
+++ powershell/02_hsvar.ps1
@@ -0,0 +1,30 @@
+# HS VaR (Beta 적용) 자동화 작업 연동 배치 스크립트
+
+$baseDate = $args[0]
+
+$GetSeqUri = 'http://localhost:5000/v2/flow/sequence/@sequence,HS_VaR_Beta,M@'
+$Sequence = Invoke-RestMethod -Method 'Get' -Uri $GetSeqUri -UseDefaultCredential
+
+$headers = @{
+    'Content-Type' = 'application/json'
+    'Accept' = 'application/json'
+}
+
+$Body = '{
+    flow_id: 164,
+    operator_params: "[{\"default\":\"20210531\",\"type\":\"date\",\"value\":\"' + $baseDate + '\",\"value_name\":\"@BaseDate\",\"name\":\"기준일\",\"hide\":false},{\"default\":\"@sequence,N\",\"type\":\"text\",\"value\":\"N\",\"value_name\":\"@RWA_YN\",\"name\":\"지주전송월작업여부\",\"hide\":false},{\"default\":\"BETA\",\"type\":\"text\",\"value\":\"BETA\",\"value_name\":\"@Path\",\"name\":\"생성폴더\",\"hide\":false},{\"default\":\"@sequence,HS_VaR_Beta,M@\",\"type\":\"text\",\"value\":\"' + $Sequence + '\",\"value_name\";\"@seq\",\"name\":\"seq\",\"hide\":false}]"
+    }'
+
+$Uri = 'http://localhost:5000/v2/flowrun/batch/create'
+$FlowId = Invoke-RestMethod -Method 'Post' -Body $body -Uri $Uri -UseDefaultCredential -Headers $headers
+$UriStatus = 'http://localhost:5000/v2/flowrun/batch/status/' + $FlowId
+$Status = "Pending"
+
+Do
+{
+    Start Sleep -Seconds 5
+    $Status = Invoke-RestMethod -Method 'Get' -Uri $UriStatus -UseDefaultCredential -Headers $headers
+
+} While ($Status -eq "pending" -or $Status -eq "inprogress")
+
+Write-Output $Status
