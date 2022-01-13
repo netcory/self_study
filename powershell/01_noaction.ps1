cccd9fb6897ad7d719c2dfedf22a6200b4e3cb97
Liam Choi <netcory@gmail.com>
2022-01-13 18:12:18 +0900

    Add files via upload

---
+++ powershell/01_noaction.ps1
@@ -0,0 +1,38 @@
+# 가상손익 자동화 작업 연동 배치 스크립트
+
+$baseDate = $args[0]
+
+$GetWorkDayUri = 'http://localhost:5000/v2/flow/workday/before/' + $baseDate
+$baseDate = Invoke-RestMethod -Method 'Get' -Uri $GetWorkDayUri -UseDefaultCredential
+
+$GetSeqUri = 'http://localhost:5000/v2/flow/sequence/@date,2@'
+$Sequence = Invoke-RestMethod -Method 'Get' -Uri $GetSeqUri -UseDefaultCredential
+
+$headers = @{
+    'Content-Type' = 'application/json'
+    'Accept' = 'application/json'
+}
+
+$Body = '{
+    flow_id: 1040,
+    operator_params: "[{\"type\":\"text\",\"value\":\"' + $baseDate + '\",\"value_name\":\"@BaseDate\",\"name\":\"기준일자\",\"hide\":false},
+                       {\"type\":\"text\",\"value\":\"N\",\"value_name\":\"@RWA_YN\",\"name\":\" 지주전송월데이터여부\",\"hide\":false},
+                       {\"type\":\"text\",\"value\":\"NOACTION\",\"value_name\":\"@Path\",\"name\":\"생성폴더\",\"hide\":true},
+                       {\"type\":\"text\",\"value\":\"' + $Sequence + '\",\"value_name\":\"@Seq\",\"name\":\"seq\",\"hide\":true}]"
+}'
+
+$PostParam = [System.Text.Encoding]::UTF8.GetBytes($Body)
+
+$Uri = 'http://localhost:5000/v2/flowrun/batch/create'
+$FlowId = Invoke-RestMethod -Method 'Post' -Body $PostParam -Uri $Uri -UseDefaultCredential -Headers $headers
+$UriStatus = 'http://localhost:5000/v2/flowrun/batch/status/' + $FlowId
+$Status = "Pending"
+
+Do
+{
+    Start Sleep -Seconds 5
+    $Status = Invoke-RestMethod -Method 'Get' -Uri $UriStatus -UseDefaultCredential -Headers $headers
+
+} While ($Status -eq "pending" -or $Status -eq "inprogress")
+
+Write-Output $Status
