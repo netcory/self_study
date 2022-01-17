43ca442dac2124d83a1ba5d268b1481767148e5b
Liam Choi <netcory@gmail.com>
2022-01-17 23:58:35 +0900

    Add files via upload

---
+++ studyC/const를_사용한_변수.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int income = 0;					// 소득액 초기화
+	double tax;						// 세금
+	const double tax_rate = 0.12;	// 세율 초기화
+	//tax_rate = 0.15; const로 변수를 선언하면 값을 바꿀 수 없으므로 에러 발생
+
+
+	income = 456;					// 소득액 저장
+	tax = income * tax_rate;		// 세금 계산
+	printf("세금은 : %.1lf입니다.\n", tax);
+
+	return 0;
+}
